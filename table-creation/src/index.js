425b72ce05f4dbc87f988f8fe227304cb30fb9c0
karthikeyan_R <karthikeyan15061998@gmail.com>
2019-11-24 20:12:13 +0530

    Add files via upload

--- table-creation/src/index.js
+++ table-creation/src/index.js
@@ -59,34 +59,44 @@ class App extends Component {
            postVal : ""
         });
     }
+
+    createTable() {
+        alert("welcome")
+    }
+
 render() {
     return(
         <div className="">
-        <div className="heading">
-          <h1>Add Table Column</h1>
+            <div className="Table-Creation">
+                <div className="heading">
+                    <h1>Insert Column Name</h1>
+                </div>
+                <div className="table-header align-ath">
+                    <div>
+                       <input className="align-cname" type="text" placeholder="Column Name" name="cname" />
+                    </div>
+                    <div className="align-ctype">
+                        <select name="ctype">
+                            <option value="0">Select Column Type</option>
+                            <option value="Date">Date</option>
+                            <option value="Number">Number</option>
+                            <option value="Multiselect">Multiselect</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="add-header" onClick={this.addcnamectype}>+</div>
+                <div class="min-header"  onClick={this.mincnamectype}>-</div><br/>
+                <div id="display-table-multiple-Column">
+                    {this.tableColumn}
+                </div>
+                <div className="heading">
+                    <div className="req">
+                        <p>*Note : Please Fill All the fields as Mandatory</p>
+                    </div><br/>
+                     <button  class="btn btn-default button sub-header" onClick={this.createTable}> Create Table</button>
+                </div>
+            </div>
         </div>
-        <div className="table-header align-ath">
-          <div>
-            <input className="align-cname" type="text" placeholder="Column Name" name="cname"/>
-          </div>
-          <div className="align-ctype">
-            <select name="ctype">
-                <option value="0">Select Column Type</option>
-                <option value="Date">Date</option>
-                <option value="Number">Number</option>
-                <option value="Multiselect">Multiselect</option>
-            </select>
-          </div>
-        </div>
-        <div class="add-header" onClick={this.addcnamectype}>+</div>
-        <div class="min-header" onClick={this.mincnamectype}>-</div><br/>
-        <div id="display-table-multiple-Column">
-             {this.tableColumn}
-        </div>
-        <div className="heading">
-        <button  class="btn btn-default button sub-header"> Create Table</button>
-        </div>
-      </div>
     );
 }
 
